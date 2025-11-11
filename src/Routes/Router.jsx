a858c669033752a80d4d4b0aee445ec1c0a4d279
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-11-11 14:29:33 +0100

    Firebase, Auth, routes

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+
+import PrivateRoute from "./PrivateRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
