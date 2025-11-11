24d142e1cb638f97e2326c51417d643e85b90f84
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-11-11 13:24:59 +0100

    movie-matrix-client

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Vite + React</h1>
+
+      <h2 className="text-3xl">This is my project</h2>
+      <button class="btn btn-neutral">Neutral</button>
+    </>
+  );
+}
+
+export default App;
